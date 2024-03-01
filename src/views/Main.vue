13462a60907f830c734fbfe51a24d3575b1b54d7
Jonas Peterburs <j.peterburs@protonmail.com>
2024-03-01 19:57:04 +0000

    Readd vue-router

---
+++ src/views/Main.vue
@@ -0,0 +1,99 @@
+<template>
+  <div>
+    <h1>Button</h1>
+    <div v-if="allAlbums.length > 0 && !loading">
+      <button @click="getRandomEpisode">Click me!</button>
+    </div>
+
+    <div v-if="selectedAlbum">
+      <h2>Selected Album</h2>
+      <p>Name: {{ selectedAlbum.name }}</p>
+      <p>Release Date: {{ selectedAlbum.release_date }}</p>
+      <p>Total Tracks: {{ selectedAlbum.total_tracks }}</p>
+
+      <div v-if="selectedAlbum.images && selectedAlbum.images.length > 0">
+        <h3>Album Artwork</h3>
+        <img v-for="(image, index) in selectedAlbum.images" :key="index" :src="image.url" :alt="`Album Image ${index + 1}`" />
+      </div>
+    </div>
+
+    <div v-if="loading">Loading...</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      allAlbums: [],
+      selectedAlbum: null,
+      loading: false
+    }
+  },
+  mounted() {
+    this.fetchAllAlbums()
+  },
+  methods: {
+    async fetchAllAlbums() {
+      try {
+        this.loading = true
+
+        const spotify_api_endpoint = 'https://api.spotify.com/v1/artists/3meJIgRw7YleJrmbpbJK6S/albums?include_groups=album&limit=50'
+        const initialResponse = await fetch(
+          spotify_api_endpoint,
+          {
+            headers: {
+              Authorization: `Bearer ${process.env.SPOTIFY_API_KEY}`
+            }
+          }
+        )
+        const initialData = await initialResponse.json()
+        const totalAlbums = initialData.total
+
+        if (totalAlbums <= 50) {
+          this.allAlbums = initialData.items
+        }
+        else {
+          const numberOfRequests = Math.ceil(totalAlbums / 50)
+
+          const promises = Array.from({ length: numberOfRequests - 1 }, (_, index) => {
+            const offset = index + 1
+
+            return fetch(
+              `${spotify_api_endpoint}&offset=${offset * 50}`,
+              {
+                headers: {
+                  Authorization: `Bearer ${process.env.SPOTIFY_API_KEY}`
+                }
+              }
+            ).then(response => response.json())
+          })
+
+          const responses = await Promise.all(promises)
+
+          this.allAlbums = [
+            ...initialData.items,
+            ...responses.reduce((albums, response) => [...albums, ...response.items], [])
+          ]
+        }
+      }
+      catch (error) {
+        console.error(error)
+      }
+      finally {
+        this.loading = false
+      }
+    },
+    async getRandomEpisode() {
+      this.loading = true
+
+      this.selectedAlbum = this.allAlbums[Math.floor(Math.random() * this.allAlbums.length)]
+
+      this.loading = false
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
